bc578d922082f4cfd5ddd07ee8d7108afa033803
marcus niihau-ashby <marcusniihauashby@gmail.com>
2025-07-01 13:40:05 -0700

    add BoarLogic

--- Assets/BoarLogic.cs
+++ Assets/BoarLogic.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class BoarLogic : MonoBehaviour
 {
 
-    public gameObject player;
+    public rigidBody myRigidBody;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
